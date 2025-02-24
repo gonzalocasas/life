393ec884901cfb882eab649d6660f0a71fe13ee6
Gonzalo Casas <casas@arch.ethz.ch>
2025-02-24 01:53:24 +0100

    Fix settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gcasas.github.io',
-  base: '/4000-weeks',
+  site: 'https://gonzalocasas.github.io/',
+  base: '/weeks',
 });
